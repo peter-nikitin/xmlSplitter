629fba1efad763a2bc2d15d4d19ab78389923b0d
Petr <nikitin.p.94@gmail.com>
2020-08-28 16:16:20 +0300

    добавил проверку расширения файлов при получении списка в папке

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const xmlsplit = new XmlSplit(batchSize = 50000, tagName = "customer")
 // ---------------------
 // СЮДА ВВЕДИ НАЗВАНИЕ ПАПКИ ИЗ КОТОРОЙ НАДО ВЗЯТЬ И КУДА НАДО ПОЛОЖИТЬ
 const settings = {
-  "INPUT_FOLDER_NAME": "/data/DetmirRu - Отчет по задаче 71121 - 27.08.2020 20-10",
-  "OUTPUT_FOLDER_NAME": "/data/DetmirRu-small-chunks"
-}
-// ДАЛЬШЕ ЛУЧШЕ НИЧЕГО НЕ ТРОГАТЬ
-// ----------------------
+    "INPUT_FOLDER_NAME": "/data/test",
+    "OUTPUT_FOLDER_NAME": "/data/DetmirRu-small-chunks"
+  }
+  // ДАЛЬШЕ ЛУЧШЕ НИЧЕГО НЕ ТРОГАТЬ
+  // ----------------------
 
 const saveFile = (path, data) => {
   fs.writeFile(path, data, function(err, data) {
@@ -32,42 +32,50 @@ const splitFile = (index, array) => {
   let nexIndex = index + 1;
   const fileName = array[index];
   let chunckNumber = 0;
+  console.log(fileName);
   var inputStream = fs.createReadStream(path.join(__dirname, `${settings.INPUT_FOLDER_NAME}/${fileName}`)) // from somewhere
 
   if (!fs.existsSync(path.join(__dirname, settings.OUTPUT_FOLDER_NAME))) {
     fs.mkdirSync(path.join(__dirname, settings.OUTPUT_FOLDER_NAME));
   }
 
-   inputStream
+  inputStream
     .pipe(xmlsplit)
     .on('data', function(data) {
       var xmlDocument = data.toString()
-      
-      
+
       const outPutFileName = `${fileName.split(".")[0]}-chunck-${chunckNumber}.xml`;
       const outputFolder = `/${settings.OUTPUT_FOLDER_NAME}/${fileName.split(".")[0]}-chuncks`;
       const outputFilePath = `/${outputFolder}/${outPutFileName}`;
-      
+
       if (!fs.existsSync(path.join(__dirname, outputFolder))) {
         fs.mkdirSync(path.join(__dirname, outputFolder));
       }
-      
+
       saveFile(path.join(__dirname, outputFilePath), xmlDocument);
 
       chunckNumber = chunckNumber + 1;
 
     }).on("end", () => {
       console.log(`Done ${chunckNumber} chuncks`);
-      splitFile(nexIndex, array);
+      if (nexIndex < array.length) {
+
+        splitFile(nexIndex, array);
+      }
     });
 }
 
 
 const getFilesFromFolder = (folder) => {
   const arrayOfItems = [];
-
   const filesInFolder = fs.readdirSync(path.join(__dirname, folder));
-  filesInFolder.forEach((file) => arrayOfItems.push(file))
+
+  filesInFolder.forEach((file) => {
+    if (path.extname(file) === ".xml") {
+      arrayOfItems.push(file)
+    }
+  })
+
 
   console.log(`Get ${filesInFolder.length} files& Start spliting`);
   return arrayOfItems;
@@ -77,8 +85,9 @@ const getFilesFromFolder = (folder) => {
 const getFilesAndSplit = (settings) => {
   const filesArray = getFilesFromFolder(settings.INPUT_FOLDER_NAME);
 
-  splitFile(0, filesArray)
-  console.log("End Splitting");
+  if (filesArray.length > 0) {
+    splitFile(0, filesArray);
+  }
 }
 
 
