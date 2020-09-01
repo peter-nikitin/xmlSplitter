0cf9c8009cbeccf436b5f72d950fdb2a71e0fce2
Petr <nikitin.p.94@gmail.com>
2020-09-01 23:46:10 +0300

    добавил функции и всяое

--- controllers/FtpController.js
+++ controllers/FtpController.js
@@ -2,6 +2,8 @@ const Client = require('ftp');
 const path = require('path');
 const fs = require('fs')
 const async = require('async');
+const unzipper = require('unzipper');
+const iconv = require("iconv-lite");
 
 
 const FTP_STATUSES = {
@@ -10,7 +12,7 @@ const FTP_STATUSES = {
 }
 
 class FtpController {
-  constructor() {
+  constructor(settings) {
     this.host = process.env.FTP_HOST;
     this.user = process.env.FTP_USER;
     this.password = process.env.FTP_PASS;
@@ -20,6 +22,7 @@ class FtpController {
     this.filesInFolder = [];
     this.downloadedFiles = 0;
     this.targetLocalFolder = "";
+    this.settings = settings;
   }
 
   init() {
@@ -47,8 +50,8 @@ class FtpController {
       this.ftpClient.list(targetFtpPath, false, (err, list) => {
         if (err) rejects(err);
         list.forEach((item) => {
-          if (path.extname(item.name) === ".xml") {
-            this.filesInFolder.push(item);
+          if (path.extname(item.name) === ".xml" || path.extname(item.name) === ".gz") {
+            this.filesInFolder.push(iconv.decode(item.name, "win1251"));
           }
         });
         resolve(this.filesInFolder);
@@ -60,10 +63,13 @@ class FtpController {
     if (!fs.existsSync(path.join(__dirname, `../${targetPath}`))) {
       fs.mkdirSync(path.join(__dirname, `../${targetPath}`));
     }
-
+    console.log(`${this.targetFileOnFtp}/${targetFile}`);
     this.ftpClient.get(`${this.targetFileOnFtp}/${targetFile}`, (err, stream) => {
-      stream.once('close', () => { this.ftpClient.end(); });
-      stream.pipe(fs.createWriteStream(`${targetPath}/${targetFile}`));
+      console.log(stream);
+      console.log(err);
+      stream.pipe(path.extname(targetFile) === ".gz" ? unzipper.Extract({ path: `${targetPath}/${targetFile}` }) : fs.createWriteStream(`${targetPath}/${targetFile}`));
+
+
       stream.on('end', () => {
         this.downloadedFiles += 1;
         console.log(`done ${targetFile}`)
@@ -72,31 +78,53 @@ class FtpController {
     });
   }
 
-  uploadFile(file, pathOnFTP, collback) {
-    console.log(pathOnFTP);
-    this.ftpClient.append(file, pathOnFTP, (err) => {
-      if (err) throw err;
-      this.ftpClient.end();
-      collback();
-    });
-  }
-
   downloadAllFilesFromFolder(targetLocalFolder) {
     if (this.targetLocalFolder === "") {
       this.targetLocalFolder = targetLocalFolder;
     }
-    return async.mapSeries(this.filesInFolder, (item, collback) => this.downloadFile(item.name, this.targetLocalFolder, collback))
+    return async.mapSeries(this.filesInFolder,
+      (item, collback) => this.downloadFile(item, this.targetLocalFolder, collback))
+  }
+
+  checkFolder(folder, targetPath) {
+    const foldersOnFtp = [];
+    return new Promise((resolve, reject) => {
+      this.ftpClient.list(targetPath, false, (err, list) => {
+        if (err) reject(err);
+        list.forEach((item) => {
+          if (item.type === "d") {
+            foldersOnFtp.push(item.name);
+          }
+        })
+        resolve(foldersOnFtp.includes(folder));
+      })
+    })
+  }
+
+  makeFolder(folderName) {
+    this.ftpClient.mkdir(folderName, (err) => {
+      if (err) throw err;
+    })
   }
 
   uploadAllFilesFromLocalFolder(filesInFolder, pathOnFTP) {
     return async.mapSeries(
       filesInFolder,
       (item, collback) => {
-        console.log(item);
         return (
-          this.uploadFile(item, pathOnFTP, collback)
+          this.uploadFile(path.join(__dirname, `/${this.settings.OUTPUT_FOLDER_NAME}/${item}`), `${pathOnFTP}/${item}`, collback)
         )
       })
+  }
+
+  uploadFile(file, pathOnFTP, collback) {
+    console.log(file);
+    return new Promise((resolve, reject) => {
+      this.ftpClient.append(file, pathOnFTP, (err) => {
+        if (err) throw err;
+        resolve(collback());
+      });
+    })
   }
 }
 
