d59b84badf168470959d9567929d71621b158e7a
Petr <nikitin.p.94@gmail.com>
2020-09-02 11:36:25 +0300

    Уже можно запускать и грузить результат на ФТП

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const path = require('path');
 const SplitController = require('./controllers/splitController');
 const FtpController = require('./controllers/FtpController');
 const getFilesFromFolder = require('./utils/getFilesFromFolder');
@@ -18,17 +19,38 @@ const settings = {
   // ----------------------
 
 const FTP = new FtpController(settings);
+const splitter = new SplitController(getFilesFromFolder(settings.INPUT_FOLDER_NAME), settings);
 
-const targetFolderOnFtp = "/Test divide";
-// const targetFolderLocal = "./data/FromFTP";
 
-// console.log(FTP.uploadFile('data/test-chuncks/0523-chunck-0.xml', '/test-chuncks/0523-chunck-0.xml', () => {}));
 
-FTP.targetFileOnFtp = '';
-// FTP.downloadFile('CustomerData/1 из 1 - DetmirRu - 1 - 01.09.2020 08-57 - 73726 - 85d32950-962f-4fed-99ec-79e5c20d2600.xml.gz', `./${settings.INPUT_FOLDER_NAME}`, () => {})
-FTP.downloadFile('CustomerData/test.xml.gz', `./${settings.INPUT_FOLDER_NAME}`, () => {})
+splitter.getFileAndSplit()
+  .then(() => console.log(`finish splitting files ${getFilesFromFolder(settings.OUTPUT_FOLDER_NAME).join()}`))
+  .then(() => FTP.checkFolder(settings.OUTPUT_FOLDER_NAME_ON_FTP, "/"))
+  .then((isFound) => {
+    if (!isFound) return FTP.makeFolder(`/${settings.OUTPUT_FOLDER_NAME_ON_FTP}`);
+    return Promise.resolve();
+  })
+  .then(() => console.log(`start uploading files ${getFilesFromFolder(settings.OUTPUT_FOLDER_NAME).join()}`))
+  .then(() => FTP.uploadAllFilesFromLocalFolder(
+    getFilesFromFolder(settings.OUTPUT_FOLDER_NAME),
+    `/${settings.OUTPUT_FOLDER_NAME_ON_FTP}`))
+  .then(() => console.log("done"))
+  .catch((err) => console.log(err))
 
-// FTP.getFilesInFolder(`/${settings.INPUT_FOLDER_NAME_ON_FTP}`)
+
+
+
+
+
+// НИЖЕ ВСЯКИЙ ТЕСТОВЫЙ БРЕД
+// ----------------------
+
+// FTP.downloadFile('CustomerData', '1 èç 1 - DetmirRu - Êëèåíòû - 31.08.2020 08-56 - 73490 - 2d59dac3-d745-4dce-9271-211d19d83864.xml.gz', settings.INPUT_FOLDER_NAME, () => {})
+// FTP.downloadFile('CustomerData/test.xml.gz', path.join(__dirname, `${settings.INPUT_FOLDER_NAME}`), () => {})
+
+// FTP.getFilesInFolder(`/CustomerData`)
+//   .then((items) => console.log(items))
+
 //   .then(() => FTP.downloadAllFilesFromFolder(settings.INPUT_FOLDER_NAME))
 //   .then(() => console.log(`finish downloading files ${getFilesFromFolder(settings.INPUT_FOLDER_NAME).join()}`))
 // .then(() => new SplitController(getFilesFromFolder(settings.INPUT_FOLDER_NAME), settings).getFileAndSplit())
