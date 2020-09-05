401493c47a1d52945f6cbd27a0c78c76a104bd8e
Petr <nikitin.p.94@gmail.com>
2020-09-05 23:29:16 +0300

    сделал основу для сервера

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 require("dotenv").config();
-const FtpController = require("./controllers/FtpController");
-const ApiController = require("./controllers/ApiController");
-const SplitController = require("./controllers/splitController");
 const CronController = require("./controllers/CronController");
+const DbController = require("./controllers/DbController");
+// const db = require("./db/index");
+
+// console.log(db);
 
 // ---------------------
 // СЮДА ВВЕДИ НАЗВАНИЕ ПАПКИ ИЗ КОТОРОЙ НАДО ВЗЯТЬ И КУДА НАДО ПОЛОЖИТЬ
 
-const customerSettings = {
-  OUTPUT_FOLDER_NAME_ON_FTP: "test-chuncks",
-  TAG_NAME: "customer",
-  ITEMS_PER_CHUNCK: 50000,
-  OPERATION_NAME: "exportSegmentsPart3",
-  EXPORT_PERIOD_HOURS: 24,
-  CRON_TIME: "0 39 18 * * *",
-};
+// const customerSettings = {
+//   NAME: "customers",
+//   OUTPUT_FOLDER_NAME_ON_FTP: "test-chuncks",
+//   TAG_NAME: "customer",
+//   ITEMS_PER_CHUNCK: 50000,
+//   OPERATION_NAME: "exportSegmentsPart3",
+//   EXPORT_PERIOD_HOURS: 24,
+//   CRON_TIME: "0 39 18 * * *",
+// };
 
-const cron = new CronController(customerSettings);
+// const db = new DbController("./db/db.js");
+// console.log(db.getOperations());
+
+// const cron = new CronController(customerSettings);
