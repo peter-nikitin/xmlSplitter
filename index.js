24a7d41a92c720525c3082057834e0126fd4f11c
Petr <nikitin.p.94@gmail.com>
2020-09-06 23:11:28 +0300

    эксперементы

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
-const CronController = require("./controllers/CronController");
+const CronModel = require("./models/CronModel");
+const CronController = require("./controllers/");
 const DbController = require("./controllers/DbController");
 // const db = require("./db/index");
 
@@ -8,17 +9,18 @@ const DbController = require("./controllers/DbController");
 // ---------------------
 // СЮДА ВВЕДИ НАЗВАНИЕ ПАПКИ ИЗ КОТОРОЙ НАДО ВЗЯТЬ И КУДА НАДО ПОЛОЖИТЬ
 
-// const customerSettings = {
-//   NAME: "customers",
-//   OUTPUT_FOLDER_NAME_ON_FTP: "test-chuncks",
-//   TAG_NAME: "customer",
-//   ITEMS_PER_CHUNCK: 50000,
-//   OPERATION_NAME: "exportSegmentsPart3",
-//   EXPORT_PERIOD_HOURS: 24,
-//   CRON_TIME: "0 39 18 * * *",
-// };
+const customerSettings = {
+  NAME: "customers",
+  OUTPUT_FOLDER_NAME_ON_FTP: "test-chuncks",
+  TAG_NAME: "customer",
+  ITEMS_PER_CHUNCK: 50000,
+  OPERATION_NAME: "exportSegmentsPart3",
+  EXPORT_PERIOD_HOURS: 24,
+  CRON_TIME: "0 39 18 * * *",
+};
 
 // const db = new DbController("./db/db.js");
 // console.log(db.getOperations());
 
-// const cron = new CronController(customerSettings);
+const cron = new CronModel(customerSettings);
+console.log(cron.getNextDate());
