0006b3e09e8a1db7d1424868c95efb2c43f9680a
Petr Nikitin <nikitin.p.94@gmail.com>
2020-10-18 23:35:36 +0300

    перенес все в src

--- db/index.js
+++
@@ -1,4 +0,0 @@
-const path = require("path");
-const DbController = require("../controllers/DbController");
-
-module.exports = new DbController(path.join(__dirname, "../db/db.json"));
--- utils/cron.js
+++
@@ -1,7 +0,0 @@
-const CronController = require("../controllers/CronController");
-
-const db = require("../db/index");
-
-const operations = db.getOperations();
-
-operations.map((item) => CronController.setCronJob(item));
