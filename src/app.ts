452f4d440ddc0bf83c6841a169b6461ffa85a382
Petr Nikitin <nikitin.p.94@gmail.com>
2020-10-19 21:33:40 +0300

    поправил адрес статики

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "./splitter-front/build")));
+app.use(express.static(path.join(__dirname, "../splitter-front/build")));
 // app.use(cors());
 // app.set("etag", false);
 
