25a934c1c59785f55fe62e325aeb7ac3f78d194a
Petr Nikitin <nikitin.p.94@gmail.com>
2020-10-19 21:17:27 +0300

    поправил запуск dotenv

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,15 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import { Request, Response, NextFunction } from "express";
+import { config } from "dotenv";
 
-require("dotenv").config();
 import bodyParser from "body-parser";
 import indexRouter from "./routes/index";
 import logsRouter from "./routes/logs";
 import operationRouter from "./routes/operation";
-import executeRouter from "./routes/execute";
+// import executeRouter from "./routes/execute";
+
+config();
 
 const app = express();
 
@@ -24,13 +26,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "./splitter-front/build")));
-app.use(cors());
-app.set("etag", false);
+// app.use(cors());
+// app.set("etag", false);
 
-// app.use("/", indexRouter);
+app.use("/", indexRouter);
 app.use("/operation", operationRouter);
 app.use("/logs", logsRouter);
-app.use("/execute", executeRouter);
+// app.use("/execute", executeRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -38,14 +40,17 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res) {
+app.use(function (req, res) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  res.status(502);
+  res.type("txt").send("Not found");
+
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get("env") === "development" ? err : {};
+  // // render the error page
+  // res.status(err.status || 500);
+  // res.render("error");
 });
 
-module.exports = app;
+export default app;
