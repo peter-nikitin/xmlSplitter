d56d9c221f6e1c1e6aa61abee7d4ac09a578b56e
Petr Nikitin <nikitin.p.94@gmail.com>
2020-10-20 19:14:53 +0300

    убрал мусор

--- splitter-front/src/App.js
+++ splitter-front/src/App.js
@@ -5,7 +5,6 @@ import "semantic-ui-css/semantic.min.css";
 
 import { Container, Header } from "semantic-ui-react";
 
-import ButtonExampleButton from "./components/btn";
 import OperationsList from "./components/operationsList";
 
 function App() {
