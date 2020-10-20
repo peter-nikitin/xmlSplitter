c99ab5f794732bb856067956bfe4eeaa87f41fc5
Petr Nikitin <nikitin.p.94@gmail.com>
2020-10-20 19:15:48 +0300

    поправил инициализацию крон

--- src/utils/cron.ts
+++ src/utils/cron.ts
@@ -5,6 +5,5 @@ import db from "../db/index";
 import { Settings } from "../declare/types.d";
 
 const operations = db.getOperations();
-const cronInstance = new CronController();
 
-operations.map((item: Settings) => cronInstance.setCronJob(item));
+operations.map((item: Settings) => CronController.setCronJob(item));
