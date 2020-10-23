d932708304edfc42abb95aa7d1cd95d9ad40567b
Petr Nikitin <nikitin.p.94@gmail.com>
2020-10-23 23:25:49 +0300

    упрастил модель крона

--- src/models/CronModel.ts
+++ src/models/CronModel.ts
@@ -1,5 +1,5 @@
 import { CronJob } from "cron";
-import FtpController from "../controllers/FtpController";
+import FtpController from "./FtpModel";
 import ApiController from "../controllers/ApiController";
 import SplitController from "../controllers/splitController";
 import db from "../db";
@@ -8,31 +8,22 @@ import { Settings } from "../declare/types.d";
 
 class CronModel {
   settings: Settings;
-  cronTime: string;
-  onComplete: null;
-  start: boolean;
-  timeZone: string;
-  context: null;
-  runOnInit: boolean;
+  onComplete: null = null;
+  start: boolean = true;
+  timeZone: string = "Europe/Moscow";
+  context: null = null;
+  runOnInit: boolean = false;
   FTP: FtpController;
   splitController: SplitController;
   apiController: ApiController;
   job: CronJob;
+  tick: () => void;
 
-  constructor(settings: Settings) {
+  constructor(settings: Settings, tickFunction: () => void) {
     this.settings = settings;
-    this.cronTime = settings.cronTimerString;
-    this.onComplete = null;
-    this.start = true;
-    this.timeZone = "Europe/Moscow";
-    this.context = null;
-    this.runOnInit = false;
-    this.FTP = new FtpController(this.settings);
-    this.splitController = new SplitController(this.settings);
-    this.apiController = new ApiController(this.settings, this.FTP.uploadFile);
-    this.tick = this.tick.bind(this);
+    this.tick = tickFunction;
     this.job = new CronJob(
-      this.cronTime,
+      this.settings.cronTimerString,
       this.tick,
       this.onComplete,
       this.start,
@@ -48,34 +39,6 @@ class CronModel {
 
   getLastDate() {
     return this.job.lastDate();
-  }
-
-  tick() {
-    this.apiController
-      .startExport()
-      .then((response) =>
-        this.apiController.startCheckingExportTask(response.data.exportId)
-      )
-      .then((urlsArray) =>
-        this.apiController.downloadAllFiles(
-          urlsArray as string[],
-          this.splitController.splitFile
-        )
-      )
-      .then(() =>
-        db.saveLogs(this.settings.taskName, {
-          operation: this.settings.taskName,
-          date: new Date(),
-          data: `✅ Успешно завершено. Экспортировано, поделено, загружено на ФТП`,
-        })
-      )
-      .catch((err) =>
-        db.saveLogs(this.settings.taskName, {
-          operation: this.settings.taskName,
-          date: new Date(),
-          data: `❌ Ошибка: ${err}`,
-        })
-      );
   }
 }
 
