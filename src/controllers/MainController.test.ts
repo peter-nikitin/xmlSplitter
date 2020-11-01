678035068bd088c74a5dd76fdf2669ffb1233c84
Petr Nikitin <nikitin.p.94@gmail.com>
2020-11-01 18:25:23 +0300

    Change mocking with mockftp module

--- src/controllers/MainController.test.ts
+++ src/controllers/MainController.test.ts
@@ -1,16 +1,19 @@
 import axios from "axios";
 import MainController from "./MainController";
-import bunyan from "bunyan";
-import FtpServer from "ftp-srv";
 import sinon from "sinon";
+
 import { config as dotEndConfig } from "dotenv";
 import fs from "fs";
 import path from "path";
 
+import FtpModel from "../models/FtpModel";
+import MockFtp from "../../__mocks__/mockFtpServer";
+import sinonStubs from "../../__mocks__/sinonStubs";
+
 const settings = {
-  taskName: "customers-test",
+  taskName: "mainController-test",
   outputPath: "test",
-  tagName: "customer",
+  tagName: "mainController-test",
   itemsPerChunk: 50000,
   operationName: "TestovyjEksportKlientov",
   exportPeriodHours: 24,
@@ -20,67 +23,22 @@ const settings = {
 jest.mock("axios");
 
 describe("exportAndUpload", () => {
-  const logger = bunyan.createLogger({ name: "test-ftp" });
   const clientDirectory = `${process.cwd()}/test_tmp`;
-  let main: MainController;
+  const mockFtp = new MockFtp(clientDirectory, settings.taskName);
+  let client: FtpModel;
 
-  const checkTestDir = (path: string) => {
-    if (!fs.existsSync(path)) {
-      fs.mkdir(path, (err) => {
-        throw err;
-      });
-    }
-  };
-
-  checkTestDir(clientDirectory);
-
-  let connection;
   const sandbox = sinon.createSandbox();
-  let server: FtpServer;
-  dotEndConfig();
 
-  beforeAll(() => {
-    sandbox.stub(process.env, "FTP_HOST").value("127.0.0.1");
-    sandbox.stub(process.env, "FTP_USER").value("test");
-    sandbox.stub(process.env, "FTP_PASS").value("test");
-    sandbox.stub(process.env, "FTP_PORT").value("8802");
-    startServer({
-      url: `ftp://${process.env.FTP_HOST}:${process.env.FTP_PORT}`,
-    });
+  sinonStubs(sandbox, "8803");
+  mockFtp.startServer({
+    url: `ftp://${process.env.FTP_HOST}:${process.env.FTP_PORT}`,
   });
+  mockFtp.checkTestDir(`${clientDirectory}/${settings.outputPath}`);
 
-  beforeAll(async () => {
-    main = new MainController(settings);
-  });
-
-  afterAll(async () => {
-    sandbox.restore();
-    server.close();
-  });
-
-  const startServer = (options = {}) => {
-    const server = new FtpServer({
-      ...options,
-      log: logger,
-      pasv_url: "127.0.0.1",
-      pasv_min: 8881,
-      greeting: ["hello", "world"],
-      anonymous: true,
-    });
-
-    server.on("login", (data, resolve) => {
-      connection = data.connection;
-      resolve({ root: clientDirectory });
-    });
-
-    server.on("client-error", (data) => {
-      console.log(data);
-    });
-
-    return server.listen();
-  };
+  const main = new MainController(settings);
 
   it("should invoke mock function with proper arguments", async () => {
+    jest.setTimeout(30000);
     const fileResponseMock = fs.createReadStream(
       path.resolve(__dirname, "../../__mocks__/mock-xml.xml")
     );
@@ -112,5 +70,8 @@ describe("exportAndUpload", () => {
 
     await main.exportAndUpload();
     expect(axios.post).toHaveBeenCalledTimes(2);
+
+    sandbox.restore();
+    mockFtp.server.close();
   });
 });
