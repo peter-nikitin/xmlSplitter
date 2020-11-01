d8666e0484a41525ae446c9c333f979e365f0c39
Petr Nikitin <nikitin.p.94@gmail.com>
2020-11-01 18:29:00 +0300

    Change mock settgins
    For better logs of mock ftp change settings

--- src/models/SplitterModel.test.ts
+++ src/models/SplitterModel.test.ts
@@ -1,13 +1,17 @@
+import sinon from "sinon";
 import fs from "fs";
 import MemoryStream from "memorystream";
 import path from "path";
 
 import SplitterModel from "./SplitterModel";
 import { mocksXmlString } from "../../__mocks__/mock-xml-string";
+import FtpModel from "./FtpModel";
+import MockFtp from "../../__mocks__/mockFtpServer";
+import sinonStubs from "../../__mocks__/sinonStubs";
 
 const settings = {
-  taskName: "customers-test",
-  outputPath: "test",
+  taskName: "splitModel-test",
+  outputPath: "splitModel",
   tagName: "customer",
   itemsPerChunk: 50000,
   operationName: "TestovyjEksportKlientov",
@@ -88,5 +92,43 @@ describe("splitFile", () => {
     );
 
     expect(resultData).toBe(mocksXmlString);
+  });
+});
+
+describe("splitter + ftp", () => {
+  const clientDirectory = `${process.cwd()}/test_tmp`;
+  const mockFtp = new MockFtp(clientDirectory, settings.taskName);
+
+  const sandbox = sinon.createSandbox();
+
+  sinonStubs(sandbox, "8802");
+  mockFtp.startServer({
+    url: `ftp://${process.env.FTP_HOST}:${process.env.FTP_PORT}`,
+  });
+  const client = new FtpModel();
+  mockFtp.checkTestDir(`${clientDirectory}/${settings.outputPath}`);
+
+  it("should upload files to FTP", async () => {
+    jest.setTimeout(30000);
+    await client.init();
+
+    const mockReadableStream = fs.createReadStream(
+      path.resolve(__dirname, "../../__mocks__/mock-xml.xml")
+    );
+    const splitter = new SplitterModel({ ...settings, itemsPerChunk: 1 });
+
+    await splitter.splitFile(mockReadableStream, (data, chunkNumber) => {
+      client.uploadFile(
+        `/${settings.outputPath}/${settings.taskName}-${chunkNumber}`,
+        data
+      );
+    });
+
+    expect(
+      fs.readdirSync(`${clientDirectory}/${settings.outputPath}/`).length
+    ).toBe(5);
+    await client.destroy();
+    sandbox.restore();
+    await mockFtp.server.close();
   });
 });
