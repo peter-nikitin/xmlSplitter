0889d4cc0e5ee5dfaea9f705129d94acf825a624
Petr Nikitin <nikitin.p.94@gmail.com>
2020-11-14 20:27:11 +0300

    fix test
    поправил проблему с проверкой тесто загрузки на фтп

--- src/controllers/MainController.test.ts
+++ src/controllers/MainController.test.ts
@@ -3,10 +3,13 @@ import MainController from "./MainController";
 import sinon from "sinon";
 import fs from "fs";
 import path from "path";
+import dotnev from "dotenv";
 
 import FtpModel from "../models/FtpModel";
 import MockFtp from "../../__mocks__/mockFtpServer";
 import sinonStubs from "../../__mocks__/sinonStubs";
+
+dotnev.config();
 
 const settings = {
   taskName: "mainController-test",
@@ -20,24 +23,29 @@ const settings = {
 
 jest.mock("axios");
 
-afterEach(() => {
+afterAll(() => {
   MockFtp.clearDir(`${process.cwd()}/test_tmp/${settings.outputPath}`);
 });
 
+const clientDirectory = `${process.cwd()}/test_tmp`;
+const mockFtp = new MockFtp(clientDirectory, settings.taskName);
+
+const sandbox = sinon.createSandbox();
+
+sinonStubs(sandbox, "8803");
+mockFtp.startServer({
+  url: `ftp://${process.env.FTP_HOST}:${process.env.FTP_PORT}`,
+});
+
+const main = new MainController(settings);
+
+afterAll(async () => {
+  sandbox.restore();
+  mockFtp.server.close();
+});
+
 describe("exportAndUpload", () => {
-  const clientDirectory = `${process.cwd()}/test_tmp`;
-  const mockFtp = new MockFtp(clientDirectory, settings.taskName);
-
-  const sandbox = sinon.createSandbox();
-
-  sinonStubs(sandbox, "8803");
-  mockFtp.startServer({
-    url: `ftp://${process.env.FTP_HOST}:${process.env.FTP_PORT}`,
-  });
-
-  const main = new MainController(settings);
-
-  it("should send 2 requests: start export and check", async () => {
+  it("should send 2 requests: start export and check", async (done) => {
     jest.setTimeout(30000);
     const fileResponseMock = fs.createReadStream(
       path.resolve(__dirname, "../../__mocks__/mock-xml.xml")
@@ -69,19 +77,15 @@ describe("exportAndUpload", () => {
       data: fileResponseMock,
     });
 
-    try {
-      await main.exportAndUpload();
-      expect(axios.post).toHaveBeenCalledTimes(2);
-      expect(axios.get).toHaveBeenCalledTimes(1);
+    await main.exportAndUpload();
+    expect(axios.post).toHaveBeenCalledTimes(2);
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    setTimeout(() => {
       expect(
         fs.readdirSync(`${process.cwd()}/test_tmp/${settings.outputPath}`)
           .length
       ).toBe(5);
-    } catch (error) {
-      console.log(error);
-    }
-
-    sandbox.restore();
-    mockFtp.server.close();
+      done();
+    }, 5000);
   });
 });
