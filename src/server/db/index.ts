a0b75f623a0eb17e15a585c8c5c25de2c3f0f6d3
Petr Nikitin <nikitin.p.94@gmail.com>
2021-06-01 23:09:36 +0300

    small changes

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import DbController from "./DbModel";
 
-const dbController = new DbController(path.join(__dirname, "../db/db.json"));
+const dbController = new DbController(path.join(__dirname, "./db.json"));
 
 export default dbController;
