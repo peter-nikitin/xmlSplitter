be8ef0c4f6f6de9ecc6848c58189ecdc7eb62d49
Petr Nikitin <nikitin.p.94@gmail.com>
2021-06-01 23:10:35 +0300

    refactor main file

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,16 +2,12 @@ import http from "http";
 
 import app from "./app";
 
-import "./cron/addCronTasksOnServerStart";
+import scheduleCroJobsFromDb from "./cron/scheduleCroJobsFromDb";
 
+scheduleCroJobsFromDb();
 const port = process.env.PORT || 8080;
-
-// console.log(cron);
-
 app.set("port", port);
-
 const server = http.createServer(app);
-
 server.listen(port);
 
 server.on("listening", () => console.log(`Listen on http://localhost:${port}`));
